8624b1c6aada04a609a3b91334fd026670c12e2c
Valeria <jamaisval@gmail.com>
2026-04-16 23:12:13 +0300

    add Product CRUD with EF Core db integration

--- Florive.BusinessLogic/BusinessLogic.cs
+++ Florive.BusinessLogic/BusinessLogic.cs
@@ -1,13 +1,21 @@
 ﻿using Florive.BusinessLogic.Functions.Products;
 using Florive.BusinessLogic.Interface;
+using Florive.DataAccess;
 
 namespace Florive.BusinessLogic
 {
     public class BusinessLogic
     {
+        private readonly AppDbContext _context;
+
+        public BusinessLogic(AppDbContext context)
+        {
+            _context = context;
+        }
+
         public IProduct GetProductActions()
         {
-            return new ProductFunction();
+            return new ProductFunction(_context);  
         }
 
         // TODO: добавить остальные 
---
+++ Florive.DataAccess/AppDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Florive.Domains.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Florive.DataAccess
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Product>()
+                .Property(p => p.Price)
+                .HasPrecision(18, 2);
+        }
+    }
+}
