ad06fab2dff91e3055e6c39fbc3c0e875556d30e
Valeria <jamaisval@gmail.com>
2026-04-24 23:31:27 +0300

    add User CRUD API endpoints

--- Florive.DataAccess/AppDbContext.cs
+++ Florive.DataAccess/AppDbContext.cs
@@ -6,6 +6,7 @@ namespace Florive.DataAccess
     public class AppDbContext : DbContext
     {
         public DbSet<Product> Products { get; set; }
+        public DbSet<User> Users { get; set; }
 
         public AppDbContext(DbContextOptions<AppDbContext> options)
             : base(options)
